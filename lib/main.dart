e186b3ded92f3f2f0ae6f254841181d3dc0521f6
Anthony Pena <apena5@alumni.jh.edu>
2022-07-12 14:22:25 -0400

    Created containers for contact information

--- lib/main.dart
+++ lib/main.dart
@@ -14,32 +14,92 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Column(
             children: <Widget>[
-              Container(
-                height: 100.0,
-                width: 100.0,
+              //Profile image
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/AnthonyPena.jpeg'),
+                ),
+              //Name
+              Text(
+                'Anthony Pena',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              //Position
+              Text(
+                'Software Engineer Intern'.toUpperCase(),
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal.shade100,
+                  fontSize: 13.0,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              //Phone Number
+              Card(
                 color: Colors.white,
-                child: Text('Container 1'),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: EdgeInsets.all(25.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        size: 20.0,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '+44 123 456 789',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20.0
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              SizedBox(
-                width: 20,
+              //Email
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: EdgeInsets.all(25.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.email,
+                        size: 20.0,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        'anthony.e.p3na@gmail.com',
+                        style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'Source Sans Pro',
+                            fontSize: 20.0
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.blue,
-                child: Text('Container 2'),
-              ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.red,
-                child: Text('Container 3'),
-              ),
-            ]
-          )
+            ],
+          ),
         ),
       ),
     );
